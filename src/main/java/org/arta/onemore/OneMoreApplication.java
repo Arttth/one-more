2ac3a292ddac9e23d5849c33015c1b54c0f11a7c
tumer1 <artabtth@gmail.com>
2024-09-24 23:20:49 +0500

    init project and database

---
+++ src/main/java/org/arta/onemore/OneMoreApplication.java
@@ -0,0 +1,13 @@
+package org.arta.onemore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneMoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneMoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/arta/onemore/OneMoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.arta.onemore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneMoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
